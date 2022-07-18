462d297a37ca5bd0e5f8b4eee0b17f7ef2a895bb
OB <nuor33333333@gmail.com>
2022-07-18 06:00:05 -0400

    Initial check-in of module HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            double number = double.Parse(str, CultureInfo.InvariantCulture);
+             
+            Console.WriteLine(number + 1);
+        
+        }  
+    }
+}
